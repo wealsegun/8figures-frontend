f89f5a89006bec82ffdd2a2bf894d3c235310d14
Ogunleye Olawale <wealsegun@gmail.com>
2023-01-20 00:19:27 +0100

    changes on the push

--- 8figures-frontend/src/app/app.module.ts
+++ 8figures-frontend/src/app/app.module.ts
@@ -11,6 +11,7 @@ import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { CommonModule } from '@angular/common';
 import {HttpClientModule} from '@angular/common/http';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
@@ -27,6 +28,7 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
     HttpClientModule,
     CommonModule,
     FormsModule,
+    RouterModule,
     ReactiveFormsModule,
 
     // FormsModule,
--- 8figures-frontend/src/app/contacts/contacts.component.ts
+++ 8figures-frontend/src/app/contacts/contacts.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ContactService } from '../services/contact.service';
 
 @Component({
   selector: 'app-contacts',
@@ -6,10 +7,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contacts.component.css']
 })
 export class ContactsComponent implements OnInit {
+  contact: any;
+  error!: string;
+  constructor(private service: ContactService) { }
+  ngOnInit() {
+    this.getContactList();
 
-  constructor() { }
+  }
 
-  ngOnInit(): void {
+  getContactList() {
+    this.service.getAllContact().subscribe(response => {
+      console.log(response);
+      this.contact = response;
+    });
+
+
+  }
+
+  deleteContact(id: any) {
+    this.service.deleteContact(+id).subscribe(response => {
+      console.log(response);
+      if (response) {
+        this.getContactList();
+      }
+      else {
+        this.error = "Contact deletion failed";
+      }
+    })
   }
 
 }
