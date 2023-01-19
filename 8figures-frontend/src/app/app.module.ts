bb1f2eb48fe289c24359b5022825d2092d84e2b0
Ogunleye Olawale <wealsegun@gmail.com>
2023-01-19 23:49:59 +0100

    changes

--- 8figures-frontend/src/app/app.module.ts
+++ 8figures-frontend/src/app/app.module.ts
@@ -3,11 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { ContactsComponent } from './contacts/contacts.component';
+import { CreateContactComponent } from './create-contact/create-contact.component';
+import { EditContactComponent } from './edit-contact/edit-contact.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    RegisterComponent,
+    ContactsComponent,
+    CreateContactComponent,
+    EditContactComponent
   ],
   imports: [
     BrowserModule
